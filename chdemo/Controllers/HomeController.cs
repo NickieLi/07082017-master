9dec79b388adf02af017082890c475d91bcc4427
quannengxiaobaike <quannengxiaobaike@gmail.com>
2017-08-31 23:38:46 +0800

    8/31

--- chdemo/Controllers/HomeController.cs
+++ chdemo/Controllers/HomeController.cs
@@ -16,32 +16,26 @@ namespace chdemo.Controllers
             return View();
         }
 
-        public ActionResult About(string Date,string Hours)
+        public ActionResult About(string Date, string Hours)
         {
-            ViewBag.Hours = new SelectList(db.historypressure, "Hours", "Hours");
-            var date = from d in db.historypressure
-                       select d;
-            if (!string.IsNullOrEmpty(Date))
-            {
-                date = date.Where(c => c.Date.Contains(Date));
-            }
-            if (!string.IsNullOrEmpty(Hours))
-            {
-                string hr = Convert.ToString(Hours);
-                date = date.Where(c => c.Hours.Contains(Hours));
-                return View(date);
-            }
-            else
-            {
-                return View(date);
-            }
+            SearchChartData(Date, Hours);
+            return View();
         }
+
+        //public ActionResult About(string Date, string Hours)
+        //{
+        //    SearchChartData(Date, Hours);
+        //    return View();
+        //}
+
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            er_modelEntities5 dbp = new er_modelEntities5();
+            var parameter = from d in dbp.parameter
+                            select d;
 
-            return View();
+            return View(parameter);
         }
 
 
@@ -56,9 +50,6 @@ namespace chdemo.Controllers
                 DataTable = dt
             };
 
-
-
-
             //Here MyDatabaseEntities  is our dbContext
             using (er_modelEntities3 dc = new er_modelEntities3())
             {
@@ -67,9 +58,7 @@ namespace chdemo.Controllers
 
             }
 
-
             var sortbytime = data.OrderByDescending(x => x.Minutes).ToList();
-
 
             dt.AddColumn("Date", "string");
             dt.AddColumn("實際蒸汽壓力(kgf/cm2G)", "number");
@@ -88,26 +77,55 @@ namespace chdemo.Controllers
             return Content(JsonConvert.SerializeObject(chart), "application/json");
         }
 
-        //public ActionResult SearchChartData(string Date, string Hours)
-        //{
-            //ViewBag.Hours = new SelectList(db.historypressure, "Hours", "Hours");
-            //var date = from d in db.historypressure
-            //           select d;
-            //if (!string.IsNullOrEmpty(Date))
-            //{
-            //    date = date.Where(c => c.Date.Contains(Date));
-            //}
-            //return View(date);
-            //if (!string.IsNullOrEmpty(Hours))
-            //{
-            //    string hr = Convert.ToString(Hours);
-            //    return View(date.Where(s => s.Hours == hr));
-            //}
-            //else
-            //{
-            //    return View(date);
-            //}
-        //}
+        public ActionResult SearchChartData(string Date, string Hours)
+        { 
+            List<historypressure> data = new List<historypressure>();
+            var dt = new VisualizationDataTable();
+            var chart = new ChartViewModel
+            {
+                Title = "Events",
+                Subtitle = "per date",
+                DataTable = dt
+            };
+
+                var date = from d in db.historypressure
+                           select d;
+                if (!string.IsNullOrEmpty(Date))
+                {
+                    if (!string.IsNullOrEmpty(Hours))
+                    {
+                        string hr = Convert.ToString(Hours);
+                        date = date.Where(c => c.Hours.Contains(Hours));
+                    }
+                    else
+                    {
+                        date = date.Where(c => c.Date.Contains(Date));
+                    }
+                }
+                else
+                {
+                    date = from d in db.historypressure
+                           select d; ;
+                }
+
+
+            var sortbytime = date.OrderByDescending(x => x.Minutes).ToList();
+
+            dt.AddColumn("Date", "string");
+            dt.AddColumn("實際蒸汽壓力(kgf/cm2G)", "number");
+            dt.AddColumn("預測蒸汽壓力(kgf/cm2G)", "number");
+
+            int counter = 0;
+            foreach (var item in sortbytime)
+            {
+                dt.NewRow(item.Minutes, item.ActualPressure, item.EstimatePressure);
+                counter++;
+                if (counter >= 13)
+                    break;
+
+            }
+            return Content(JsonConvert.SerializeObject(chart), "application/json");
+        }
 
     }
 }
---
+++ chdemo/Models/ClassNow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace chdemo.Models
+{
+    [MetadataType(typeof(ClassNowMetadata))]
+    public partial class ClassNow
+    {
+        private class ClassNowMetadata
+        {
+            [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy/MM/dd}")]
+            public DateTime Ondate { get; set; }
+        }
+    }
+}
