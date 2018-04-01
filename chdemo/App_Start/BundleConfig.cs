d73e48d30418d01866a2b19041625e116c6e1f82
quannengxiaobaike <quannengxiaobaike@gmail.com>
2018-04-01 17:02:40 +0800

    ok

--- chdemo/App_Start/BundleConfig.cs
+++ chdemo/App_Start/BundleConfig.cs
@@ -17,7 +17,9 @@ namespace chdemo
             // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-*",
+                        "~/Scripts/plugins.js",
+                        "~/Scripts/pace.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
@@ -25,7 +27,10 @@ namespace chdemo
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/vendor.css",
+                      "~/Content/main.css",
+                      "~/Content/base.css"));
         }
     }
 }
